bc77c4c4f5e0e4280e6eabdbb209709cf91550bd
AngeloCS2C <116428273+AngeloCS2C@users.noreply.github.com>
2022-11-22 23:01:07 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String[] args) {
+		Battery battery = new Battery(27);
+		Smartphone phone = new Smartphone(battery);
+		phone.setName("Xiaomi");
+		phone.open();
+	}
+}
+
+
